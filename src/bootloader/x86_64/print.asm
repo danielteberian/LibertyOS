836a7e2e919f3077d20bb417096af2fe3c2671f6
danielteberian <danielteberian@github.com>
2021-08-22 20:47:29 -0500

    Added the print.asm file, to handle printing. Also, I renamed (and shortened) a variable for libfs.asm, which reduces the length of the code even more.

---
+++ src/bootloader/x86_64/print.asm
@@ -0,0 +1,40 @@
+SECTION .text
+USE16
+print_ln:
+	mov al, 13
+	call print_char
+	mov al, 10
+	jmp print_char
+print:
+	pushf
+	cld
+.loop:
+	lodsb
+	test al, al
+	jz .done
+	call print_char
+	jmp .loop
+.done:
+	popf
+	ret
+print_char:
+	pusha
+	mov bx, 7
+	mov ah, 0x0e
+	int 0x10
+	popa
+	ret
+print_hex:
+	mov cx, 4
+.lp:
+	mov al, bh
+	shr al, 4
+	cmp al, 0xA
+	jb .below_0xA
+	add al, 'A' - 0xA - '0'
+.below_0xA:
+	add al, '0'
+	call print_char
+	shl bx, 4
+	loop .lp
+	ret
